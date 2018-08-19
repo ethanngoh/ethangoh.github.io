f3432087fe1428d3a1fe2f190eaaa11f015ffd00
Chris Bentivenga <cbentivenga@lyft.com>
2018-08-19 09:26:20 -0700

    Force https

--- _chrisbenti_site/src/App.tsx
+++ _chrisbenti_site/src/App.tsx
@@ -5,12 +5,24 @@ import { ContactPage } from "./pages/ContactPage";
 import { IndexPage } from "./pages/IndexPage";
 
 class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    const host = "www.chrisbenti.com";
+    if (
+      window.location.host === host &&
+      window.location.protocol !== "https:"
+    ) {
+      window.location.protocol = "https:";
+    }
+  }
   public render() {
     return (
-      <BrowserRouter><>
-        <Route exact={true} path="/" component={IndexPage} />
-        <Route exact={true} path="/contact" component={ContactPage} />
-      </></BrowserRouter>
+      <BrowserRouter>
+        <>
+          <Route exact={true} path="/" component={IndexPage} />
+          <Route exact={true} path="/contact" component={ContactPage} />
+        </>
+      </BrowserRouter>
     );
   }
 }
