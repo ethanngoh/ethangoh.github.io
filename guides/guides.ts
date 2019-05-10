a66f21bdd9f8deb61ecebe88029980f2ae261235
Chris Bentivenga <cbentivenga@lyft.com>
2019-05-10 13:56:13 -0700

    more guides

--- guides/guides.ts
+++ guides/guides.ts
@@ -3,8 +3,12 @@ import * as fse from 'fs-extra'
 import { lstatSync, readdirSync, writeFileSync, writeFile } from 'fs'
 import { join } from 'path'
 
-const guides: [{ name: string; title: string; iframeSrc: string }] = [
-    { name: 'sounders-game-day', title: "Benti's Sounders Guide", iframeSrc: 'https://docs.google.com/document/d/e/2PACX-1vQsBnqALmffNgdFyVgojKtz3zGZ-Km-FugIaLUUQ-9hG_R8qKb-x5w04mFTQfzb0zmIwWklpBGfVi2s/pub?embedded=true' }
+const guides: { name: string; title: string; iframeSrc: string }[] = [
+    { name: 'sounders-game-day', title: "Benti's Sounders Guide", iframeSrc: 'https://docs.google.com/document/d/e/2PACX-1vQsBnqALmffNgdFyVgojKtz3zGZ-Km-FugIaLUUQ-9hG_R8qKb-x5w04mFTQfzb0zmIwWklpBGfVi2s/pub?embedded=true' },
+    { name: 'seattle', title: "Benti's Seattle Guide", iframeSrc: "https://docs.google.com/document/d/e/2PACX-1vTsS9eBsfCV6QePY1qBwSOTKNVcNf8UrvzoRjW_z72Xu7VgqWqm9R9bR9gCrlFZMvW03P4wisUBiqQF/pub?embedded=true" },
+    { name: 'sf', title: "Benti's SF Guide", iframeSrc: "https://docs.google.com/document/d/e/2PACX-1vT8qBXEtmqx4yj3fNHyB1gMz1BmUsM1FHsXZdnIXOcNTiJXz3KtcKZ1s2FmiC5EAkLdxo97ACSOjjqb/pub?embedded=true" },
+    { name: 'sd', title: "Benti's San Diego Guide", iframeSrc: "https://docs.google.com/document/d/e/2PACX-1vTKvaNUMQ4omcObYEq077i8PbfL4iDvOBjuVrVgAIaXcqpAbMzJhuZJat2Y51wXqKvVCsSfXE2t_kYn/pub?embedded=true" },
+    { name: 'nashville', title: "Benti's Nashville Guide", iframeSrc: "https://docs.google.com/document/d/e/2PACX-1vQPnOaYeLdfTHF4rv64Q71qfn-pF6GQrwhiphFLYpZh6ekS-KtZ1_GsDKyZ1tc9J43uaA1Btkppy4-2/pub?embedded=true" },
 ]
 
 function clean() {
