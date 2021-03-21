b583d3bb1ccfa9a3e88dcf66ee3ba6c9c4221b26
Chris Bentivenga <chris@chrisbenti.com>
2021-03-21 14:10:18 -0700

    starting from scratch

--- clock/src/SplitNumbers/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function SplitNumbers({num1, num2}){
-    return <div style={{display:'flex', textAlign: 'center'}}>
-        {(typeof num1 === 'number' && <div style={{flexGrow: 1}}>{num1}</div>)}
-        {(typeof num2 === 'number' && <div style={{flexGrow: 1}}>{num2}</div>)}
-    </div>
-}
