bc350852418d6732a479e464434822c3e9954352
Chris Bentivenga <chris@chrisbenti.com>
2021-03-27 20:00:59 -0700

    basic bones

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { ContactPage } from "./pages/ContactPage";
+import { IndexPage } from "./pages/IndexPage";
+import "./App.css";
+
+function App() {
+  return (
+    <Router basename="/">
+      <Switch>
+        <Route path="/contact">
+          <ContactPage />
+        </Route>
+        <Route path="/">
+          <IndexPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
