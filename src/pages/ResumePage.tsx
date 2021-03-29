66910621dfff4a2948594a31ce4eef11beace49c
Chris Bentivenga <chris@chrisbenti.com>
2021-03-28 21:25:36 -0700

    pretty resume page

---
+++ src/pages/ResumePage.tsx
@@ -0,0 +1,41 @@
+import { RoyalBluePage } from "../components/royalBluePage";
+import styled from "styled-components";
+import { VscFilePdf } from "react-icons/vsc";
+import { COLORS } from "../colors";
+
+const ResumeLink = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ResumeA = styled.a`
+    text-decoration: none;
+    color: ${COLORS.ROYAL_BLUE};
+    padding: 2em;
+    &:hover {
+        border: dashed 1px ${COLORS.ROYAL_BLUE};
+    }
+`;
+
+const ResumeText = styled.div`
+    margin-top: 0.3em;
+    font-size: 1.5em;
+    font-weight: semi-bold;
+`;
+
+const ResumeIcon = styled(VscFilePdf)`
+    font-size: 5em;
+`;
+
+export const ResumePage = () => (
+    <RoyalBluePage>
+        <ResumeLink>
+            <ResumeA href="https://www.chrisbenti.com/resume-content/chris-bentivenga-resume.pdf">
+                <ResumeIcon />
+                <ResumeText>(Click Me)</ResumeText>
+            </ResumeA>
+        </ResumeLink>
+    </RoyalBluePage>
+);
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,16 @@
 import { Clock } from "./pages/Clock";
 import { BlackPage } from "./pages/BlackPage";
 import { SparrowPage } from "./pages/SparrowPage";
+import { ResumePage } from "./pages/ResumePage";
 
 export const ReactRoutes = {
     "/clock": { component: <Clock />, title: "Clock" },
     "/black": { component: <BlackPage />, title: "Black Page" },
-    "/sparrow": { component: <SparrowPage />, title: "JACK SPARROW" }
+    "/sparrow": { component: <SparrowPage />, title: "JACK SPARROW" },
+    "/resume": {
+        component: <ResumePage />,
+        title: "Resume | Chris Bentivenga"
+    }
 };
 
 export const RedirectRoutes = {
